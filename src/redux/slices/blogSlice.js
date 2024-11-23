4a34c618a6e3a9f746f764dae966132f82662478
Zacky <zackwalker130903@gmail.com>
2024-11-23 20:36:18 +0700

    update blog fixes

--- src/redux/slices/blogSlice.js
+++ src/redux/slices/blogSlice.js
@@ -22,24 +22,26 @@ export const updateBlog = createAsyncThunk(
 	'blog/updateBlog',
 	async (blogData, {rejectWithValue}) => {
 		try {
-			const response = await api.post('/Blog/Update', blogData, {
+			const response = await api.put('/Blog/Update', blogData, {
 				headers: {'Content-Type': 'multipart/form-data'},
 			});
-			;
 			return response;
 		} catch (error) {
 			return rejectWithValue(error || 'Error updating blog');
 		}
 	}
 );
-
 // Async thunk for removing a blog
 export const removeBlog = createAsyncThunk('blog/removeBlog', async (blogId, {rejectWithValue}) => {
 	try {
-		const response = await api.post(`/Blog/Remove?BlogId=${blogId}`);
-		return blogId; // Return the blogId to remove from state
+		// Make a DELETE request with the blogId as a query parameter
+		const response = await api.delete(`/Blog/Remove`, {params: {BlogId: blogId}});
+
+		// Assuming the response is successful, return the blogId to remove it from the state
+		return blogId;
 	} catch (error) {
-		return rejectWithValue(error || 'Error removing blog');
+		// Handle errors and pass the error message to rejectWithValue for better error management
+		return rejectWithValue(error.response?.data?.message || 'Error removing blog');
 	}
 });
 
@@ -51,7 +53,6 @@ export const fetchAllBlogs = createAsyncThunk(
 			const response = await api.get('/Blog/Staff/All', {
 				params: {CurrentPage, PageSize},
 			});
-			;
 			return response; // Adjust based on actual API response structure
 		} catch (error) {
 			const message = error?.message || 'Error fetching blogs';
