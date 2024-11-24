306c7d33bfb550306eabc853ff683b1d197b42c8
Zacky <zackwalker130903@gmail.com>
2024-11-24 16:34:49 +0700

    temperory fix of jewelry Page

--- src/pages/Admin/ProductPage/JewelryPage/JewelryFormModal/JewelryFormModal.jsx
+++ src/pages/Admin/ProductPage/JewelryPage/JewelryFormModal/JewelryFormModal.jsx
@@ -25,21 +25,39 @@ const JewelryFormModal = ({
 		setIsCreateFormOpen(false);
 		onClose();
 	};
+	const [isMainDiamondsOpen, setIsMainDiamondsOpen] = useState(false);
+	const [isSizeMetalsOpen, setIsSizeMetalsOpen] = useState(false);
+	const [isSideDiamondsOpen, setIsSideDiamondsOpen] = useState(false);
+	const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
+	const [modelToConfirm, setModelToConfirm] = useState(null);
 
+	const handleModelSelection = (modelId) => {
+		const model = models.find((m) => m.Id === modelId);
+		setModelToConfirm(model);
+		setIsConfirmModalOpen(true);
+	};
+
+	const confirmModelSelection = () => {
+		handleModelChange(modelToConfirm.Id); // Call the original handler with confirmed model
+		setIsConfirmModalOpen(false);
+	};
+	const handleCloseConfirmation = () => {
+		setIsConfirmModalOpen(false);
+	};
 	return (
 		<>
 			<Modal
 				title="Jewelry Request Form"
 				visible={isCreateFormOpen}
-				// onOk={() => form.submit()}
+				onOk={() => form.submit()}
 				onCancel={handleCloseModal}
 				footer={null}
 				width={800}
 				centered
 			>
-				<Form form={form} layout="vertical" onFinish={handleSubmit}>
+				<Form form={form} layout="vertical" width={1600} onFinish={handleSubmit}>
 					<Form.Item label="Mẫu Trang Sức" name={['JewelryRequest', 'ModelId']}>
-						<Select onChange={handleModelChange} placeholder="Select a model">
+						<Select onChange={handleModelSelection} placeholder="Select a model">
 							{Array.isArray(models) &&
 								models.map((model) => (
 									<Option key={model.Id} value={model.Id}>
@@ -51,7 +69,6 @@ const JewelryFormModal = ({
 								))}
 						</Select>
 					</Form.Item>
-
 					<Form.Item label="Vật Liệu" name={['JewelryRequest', 'MentalId']}>
 						<Select
 							onChange={handleMetalChange}
@@ -141,7 +158,361 @@ const JewelryFormModal = ({
 							/>
 						</div>
 					)}
+					{/* Confirmation Modal */}
+					<Modal
+						title="Confirm Model Selection"
+						visible={isConfirmModalOpen}
+						onCancel={handleCloseModal}
+						footer={null}
+						centered
+						className="rounded-lg shadow-lg"
+					>
+						{modelToConfirm && (
+							<div className="fixed inset-0 bg-gray bg-opacity-50 flex justify-center items-center p-3 z-50">
+								<div className="bg-white p-4 rounded-lg shadow-lg w-11/12 md:w-1/2 lg:w-2/3">
+									<h2 className="text-3xl font-bold text-primary text-center">
+										Chi Tiết Mẫu Trang Sức
+									</h2>
+									<div className="flex gap-8">
+										{/* Left Column */}
+										<div className="flex-1">
+											<p className="text-lg font-medium text-gray">
+												<strong>Tên:</strong> {modelToConfirm.Name}
+											</p>
+											<p className="text-lg font-medium text-gray">
+												<strong>Phân Loại:</strong>{' '}
+												{modelToConfirm.Category.Name}
+											</p>
+											<p className="text-lg font-medium text-gray">
+												<strong>Mã Mẫu Trang Sức:</strong>{' '}
+												{modelToConfirm.ModelCode}
+											</p>
+											<p className="text-lg font-medium text-gray">
+												<strong>Phí Chế Tác:</strong>{' '}
+												{modelToConfirm.CraftmanFee} VND
+											</p>
+											<p className="text-lg font-medium text-gray">
+												<strong>Mô Tả:</strong> {modelToConfirm.Description}
+											</p>
+											<p className="text-lg font-medium text-gray">
+												<strong>Ngang:</strong> {modelToConfirm.Width} mm
+											</p>
+											<p className="text-lg font-medium text-gray">
+												<strong>Có Thể Khắc:</strong>{' '}
+												{modelToConfirm.IsEngravable ? 'Yes' : 'No'}
+											</p>
+										</div>
 
+										{/* Right Column */}
+										<div className="flex-1">
+											<p className="text-lg font-medium text-gray">
+												<strong>Số Lượng Kim Cương Chính:</strong>{' '}
+												{modelToConfirm.MainDiamondCount}
+											</p>
+											<p className="text-lg font-medium text-gray">
+												<strong>Số Lượng Kim Cương Tấm:</strong>{' '}
+												{modelToConfirm.SideDiamondOptionCount}
+											</p>
+											<p className="text-lg font-medium text-gray">
+												<strong>Xi Rhodium:</strong>{' '}
+												{modelToConfirm.IsRhodiumFinish ? 'Yes' : 'No'}
+											</p>
+											<p className="text-lg font-medium text-gray">
+												<strong>Hỗ Trợ các kim loại:</strong>{' '}
+												{modelToConfirm.MetalSupported.join(', ')}
+											</p>
+										</div>
+									</div>
+									{/* Main Diamonds Section */}
+									{modelToConfirm.MainDiamonds &&
+										modelToConfirm.MainDiamonds.length > 0 && (
+											<div className="mt-6 ">
+												<h3
+													className="text-xl font-semibold cursor-pointer"
+													onClick={() =>
+														setIsMainDiamondsOpen(!isMainDiamondsOpen)
+													}
+												>
+													Kim Cương Chính {isMainDiamondsOpen ? '▲' : '▼'}
+												</h3>
+												{isMainDiamondsOpen && (
+													<ul className="space-y-4 mt-3 pl-4 max-h-24 overflow-y-auto border rounded-md">
+														{modelToConfirm.MainDiamonds.map(
+															(diamond, index) => (
+																<li
+																	key={index}
+																	className="border-b pb-4"
+																>
+																	<div className="flex justify-between px-5 py-2">
+																		<div>
+																			<p className="text-gray">
+																				<strong>
+																					Mẫu Thiết Kế:
+																				</strong>{' '}
+																				{
+																					diamond.SettingType
+																				}
+																			</p>
+																		</div>
+																		<div>
+																			<p className="text-gray">
+																				<strong>
+																					Só Lượng:
+																				</strong>{' '}
+																				{diamond.Quantity}
+																			</p>
+																		</div>
+																		<div>
+																			<p className="text-gray">
+																				<strong>
+																					ID Yêu Cầu Của
+																					Kim Cương Chính:
+																				</strong>{' '}
+																				{diamond.MainDiamondReqId ||
+																					'N/A'}
+																			</p>
+																		</div>
+																		<div>
+																			{diamond.Shapes &&
+																			diamond.Shapes.length >
+																				0 ? (
+																				diamond.Shapes.map(
+																					(
+																						shape,
+																						shapeIndex
+																					) => (
+																						<div
+																							key={
+																								shapeIndex
+																							}
+																							className="pl-4 gap-3 flex justify-between"
+																						>
+																							<p className="text-gray">
+																								<strong>
+																									Hình
+																									Dáng:
+																								</strong>{' '}
+																								{shape
+																									.Shape
+																									?.ShapeName ||
+																									'N/A'}
+																							</p>
+
+																							<p className="text-gray">
+																								<strong>
+																									Khoảng
+																									Carat:
+																								</strong>{' '}
+																								{
+																									shape.CaratFrom
+																								}{' '}
+																								-{' '}
+																								{
+																									shape.CaratTo
+																								}{' '}
+																								carats
+																							</p>
+																						</div>
+																					)
+																				)
+																			) : (
+																				<p className="text-gray">
+																					Không Có Hình
+																					Dáng
+																				</p>
+																			)}
+																		</div>
+																	</div>
+																</li>
+															)
+														)}
+													</ul>
+												)}
+											</div>
+										)}
+									{/* Size Metals Section */}
+									{modelToConfirm.SizeMetals &&
+										modelToConfirm.SizeMetals.length > 0 && (
+											<div className="mt-6  ">
+												<h3
+													className="text-xl font-semibold cursor-pointer"
+													onClick={() =>
+														setIsSizeMetalsOpen(!isSizeMetalsOpen)
+													}
+												>
+													Size Metals {isSizeMetalsOpen ? '▲' : '▼'}
+												</h3>
+												{isSizeMetalsOpen && (
+													<ul className="space-y-4 mt-3 pl-4 max-h-24 overflow-y-auto  border rounded-md">
+														{modelToConfirm.SizeMetals.map(
+															(sizeMetal, index) => (
+																<li
+																	key={index}
+																	className="border-b pb-4"
+																>
+																	<div className="flex justify-between px-5 py-2">
+																		<div>
+																			<p className="text-gray">
+																				<strong>
+																					Size:
+																				</strong>{' '}
+																				{
+																					sizeMetal.Size
+																						.Value
+																				}{' '}
+																				{
+																					sizeMetal.Size
+																						.Unit
+																				}
+																			</p>
+																		</div>
+																		<div>
+																			<p className="text-gray">
+																				<strong>
+																					Metal:
+																				</strong>{' '}
+																				{
+																					sizeMetal.Metal
+																						.Name
+																				}
+																			</p>
+																		</div>
+																		<div>
+																			<p className="text-gray">
+																				<strong>
+																					Weight:
+																				</strong>{' '}
+																				{sizeMetal.Weight} g
+																			</p>
+																		</div>
+																		<div>
+																			<p className="text-gray">
+																				<strong>
+																					Price:
+																				</strong>{' '}
+																				{
+																					sizeMetal.Metal
+																						.Price
+																				}{' '}
+																				VND
+																			</p>
+																		</div>
+																	</div>
+																</li>
+															)
+														)}
+													</ul>
+												)}
+											</div>
+										)}
+									{/* Side Diamonds Section */}
+									{modelToConfirm.SideDiamonds &&
+										modelToConfirm.SideDiamonds.length > 0 && (
+											<div className="mt-6 ">
+												<h3
+													className="text-xl font-semibold cursor-pointer"
+													onClick={() =>
+														setIsSideDiamondsOpen(!isSideDiamondsOpen)
+													}
+												>
+													Side Diamonds {isSideDiamondsOpen ? '▲' : '▼'}
+												</h3>
+												{isSideDiamondsOpen && (
+													<ul className="space-y-4 mt-3 pl-4 max-h-24 overflow-y-auto border rounded-md ">
+														{modelToConfirm.SideDiamonds.map(
+															(sideDiamond, index) => (
+																<li
+																	key={index}
+																	className="border-b pb-4"
+																>
+																	<div className="flex justify-between px-5 py-2">
+																		<div>
+																			<p className="text-gray">
+																				<strong>
+																					Carat Weight:
+																				</strong>{' '}
+																				{
+																					sideDiamond.CaratWeight
+																				}
+																			</p>
+																		</div>
+																		<div>
+																			<p className="text-gray">
+																				<strong>
+																					Setting Type:
+																				</strong>{' '}
+																				{
+																					sideDiamond.SettingType
+																				}
+																			</p>
+																		</div>
+																		<div>
+																			<p className="text-gray">
+																				<strong>
+																					Quantity:
+																				</strong>{' '}
+																				{
+																					sideDiamond.Quantity
+																				}
+																			</p>
+																		</div>
+																		<div>
+																			<p className="text-gray">
+																				<strong>
+																					Color Range:
+																				</strong>{' '}
+																				{
+																					sideDiamond.ColorMin
+																				}{' '}
+																				-{' '}
+																				{
+																					sideDiamond.ColorMax
+																				}
+																			</p>
+																		</div>
+																		<div>
+																			<p className="text-gray">
+																				<strong>
+																					Clarity Range:
+																				</strong>{' '}
+																				{
+																					sideDiamond.ClarityMin
+																				}{' '}
+																				-{' '}
+																				{
+																					sideDiamond.ClarityMax
+																				}
+																			</p>
+																		</div>
+																	</div>
+																</li>
+															)
+														)}
+													</ul>
+												)}
+											</div>
+										)}
+									<div className="flex justify-end p-5">
+										<Button
+											key="cancel"
+											onClick={() => handleCloseConfirmation(false)}
+										>
+											Hủy
+										</Button>
+										,
+										<Button
+											key="confirm"
+											type="primary"
+											onClick={confirmModelSelection}
+										>
+											Chọn Mẫu Trang Sức Này
+										</Button>
+									</div>
+									,
+								</div>
+							</div>
+						)}
+					</Modal>
 					<Form.Item className="flex justify-end">
 						<Button onClick={handleCloseModal} className="mr-4">
 							Hủy
@@ -153,7 +524,7 @@ const JewelryFormModal = ({
 							}}
 							className="mx-4"
 						>
-							Clear
+							Xóa tất cả
 						</Button>
 						<Button type="primary" htmlType="submit">
 							Tạo
--- src/pages/Admin/ProductPage/JewelryPage/JewelryPage.jsx
+++ src/pages/Admin/ProductPage/JewelryPage/JewelryPage.jsx
@@ -1,8 +1,21 @@
 import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {Form, Input, Button, Checkbox, Select, Row, Col, Spin, notification, Card} from 'antd';
+import {
+	Form,
+	Input,
+	Button,
+	Checkbox,
+	Select,
+	Row,
+	Col,
+	Spin,
+	notification,
+	Card,
+	Modal,
+} from 'antd';
 import {SearchOutlined, PlusOutlined} from '@ant-design/icons';
 import {fetchAllJewelry} from '../../../../redux/slices/jewelry/jewelrySlice';
+import JewelryModelList from '../../PromotionPage/JewelryModelList';
 import {
 	selectJewelryList,
 	selectJewelryLoading,
@@ -14,15 +27,18 @@ import JewelryCreateForm from './JewelryCreateForm';
 
 const JewelryPage = () => {
 	const dispatch = useDispatch();
-	const jewelryList = useSelector(selectJewelryList);
 	const loading = useSelector(selectJewelryLoading);
 	const error = useSelector(selectJewelryError);
 	const totalPage = useSelector(selectJewelryTotalPage);
-
-	const [selectedJewelry, setSelectedJewelry] = useState(null);
-	const [isCreateFormOpen, setIsCreateFormOpen] = useState(false);
 	const [currentPage, setCurrentPage] = useState(1);
 	const [pageSize, setPageSize] = useState(5);
+
+	const jewelryList = useSelector(selectJewelryList);
+	const [isPopupVisible, setIsPopupVisible] = useState(false);
+	const [selectedModel, setSelectedModel] = useState(null);
+	const [selectedModelId, setSelectedModelId] = useState(null);
+	const [selectedJewelry, setSelectedJewelry] = useState(null);
+	const [isCreateFormOpen, setIsCreateFormOpen] = useState(false);
 
 	const [filters, setFilters] = useState({
 		ModelName: '',
@@ -33,6 +49,13 @@ const JewelryPage = () => {
 		Status: 1,
 	});
 	const {Meta} = Card;
+
+	const handleSelectModel = (model) => {
+		console.log('Model Selected in Parent:', model);
+		setSelectedModel(model);
+		setIsPopupVisible(false);
+	};
+
 	const handleFilterChange = (e) => {
 		const {name, value} = e.target;
 		setFilters({...filters, [name]: value});
@@ -41,10 +64,26 @@ const JewelryPage = () => {
 	const handleHasSideDiamondChange = (e) => {
 		setFilters({...filters, HasSideDiamond: e.target.checked});
 	};
-
 	useEffect(() => {
-		dispatch(fetchAllJewelry({CurrentPage: currentPage, PageSize: pageSize, filters}));
-	}, [dispatch, currentPage, pageSize, filters]);
+		if (selectedModel?.Id) {
+			// Fetch jewelry items based on the selected model
+			dispatch(
+				fetchAllJewelry({
+					CurrentPage: currentPage,
+					PageSize: pageSize,
+					filters,
+					JewelryModelId: selectedModel?.Id,
+				})
+			).catch((error) => {
+				notification.error({
+					message: 'Error',
+					description:
+						error?.data?.title || error?.detail || 'Failed to load jewelry items',
+					duration: 3,
+				});
+			});
+		}
+	}, [dispatch, selectedModel?.Id, currentPage, pageSize, filters]);
 
 	useEffect(() => {
 		if (error) {
@@ -83,7 +122,29 @@ const JewelryPage = () => {
 	return (
 		<div className="container mx-auto px-4">
 			<h1 className="text-4xl font-semibold text-primary mb-6">Jewelry Collection</h1>
-
+			<Form.Item
+				label="Mẫu Trang Sức"
+				name="jewelryModelId"
+				rules={[
+					{
+						required: true,
+						message: 'Vui Lòng Chọn Mẫu Trang Sức',
+					},
+				]}
+			>
+				<div className="flex items-center gap-2">
+					<Input readOnly value={selectedModel?.Id} placeholder={selectedModel?.Name} />
+					<Button onClick={() => setIsPopupVisible(true)}>Chọn Mẫu Trang Sức</Button>
+				</div>
+			</Form.Item>
+			<Modal
+				open={isPopupVisible}
+				onCancel={() => setIsPopupVisible(false)}
+				footer={null}
+				width="80%"
+			>
+				<JewelryModelList onSelectModel={handleSelectModel} />
+			</Modal>
 			{/* Filter Section */}
 			<div className="filter-section mb-6">
 				<h2 className="text-2xl font-semibold text-primary mb-4">Filters</h2>
@@ -163,49 +224,57 @@ const JewelryPage = () => {
 			{loading && <Spin tip="Loading..." className="flex justify-center mt-5" />}
 			{error && <p className="text-red-500 text-center">{error}</p>}
 			{!loading && !error && (
-				<div className="jewelry-list grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+				<div className="jewelry-list grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
 					{Array.isArray(jewelryList) &&
 						jewelryList.map((jewelry) => (
 							<div
 								key={jewelry.Id}
 								onClick={() => handleJewelryClick(jewelry)}
-								className="jewelry-item p-4 flex justify-between transition-all transform hover:scale-105"
+								className="jewelry-item bg-white p-6 rounded-lg shadow-lg transition-all transform hover:scale-105 hover:shadow-2xl"
 							>
 								<Card
 									hoverable
-									className="shadow-lg bg-tintWhite rounded-lg border border-gray-300 flex flex-row"
+									className="flex flex-col lg:flex-row bg-tintWhite rounded-lg border border-gray-200"
 								>
-									{/* Image on the Left */}
-									<div className="w-1/3 sm:w-1/4">
+									{/* Image Section */}
+									<div className="lg:w-1/3 w-full mb-4 lg:mb-0">
 										<img
 											alt={jewelry.SerialCode}
 											src={
 												jewelry.thumbnail?.mediaPath ||
 												'/default-thumbnail.jpg'
 											}
-											className="w-full h-48 object-cover rounded-md"
-											style={{boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)'}}
+											className="w-full h-48 lg:h-full object-cover rounded-lg"
+											style={{
+												boxShadow: '0 6px 12px rgba(0, 0, 0, 0.1)',
+											}}
+											loading="lazy"
 										/>
 									</div>
 
-									{/* Description on the Right */}
-									<div className="w-2/3 sm:w-3/4 p-4">
+									{/* Description Section */}
+									<div className="lg:w-2/3 w-full lg:pl-4">
 										<Meta
 											title={
-												<h2 className="text-lg font-semibold text-primary">
+												<h2 className="text-lg font-semibold text-primary mb-2">
 													{jewelry.SerialCode}
 												</h2>
 											}
 											description={
 												<div className="space-y-2">
-													<p className="text-sm text-gray-500">
+													<p className="text-sm text-gray-500 flex items-center">
+														<span className="icon-category mr-2">
+															📦
+														</span>
 														Category:{' '}
 														{jewelry.Model?.CategoryId || 'N/A'}
 													</p>
-													<p className="text-sm text-gray-500">
+													<p className="text-sm text-gray-500 flex items-center">
+														<span className="icon-metal mr-2">🛠️</span>
 														Metal: {jewelry.Metal?.Name || 'N/A'}
 													</p>
-													<p className="text-sm text-gray-500">
+													<p className="text-sm text-gray-500 flex items-center">
+														<span className="icon-price mr-2">💰</span>
 														Price:{' '}
 														{jewelry.TotalPrice > 0
 															? jewelry.TotalPrice.toLocaleString()
@@ -235,13 +304,16 @@ const JewelryPage = () => {
 															Engraving: {jewelry.EngravedText}
 														</p>
 													)}
-													<p className="text-sm font-medium text-darkGreen">
+													<p className="text-lg font-semibold text-darkGreen mt-4">
 														Total Price:{' '}
 														{jewelry.TotalPrice > 0
 															? jewelry.TotalPrice.toLocaleString()
 															: 'Contact for price'}{' '}
 														VND
 													</p>
+													<button className="mt-4 py-2 px-4 bg-primary text-white rounded-lg hover:bg-primary-dark">
+														View Details
+													</button>
 												</div>
 											}
 										/>
