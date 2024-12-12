b77134c191a8e39cc5d3ceac2c6c233cb1dcfc91
Zacky <zackwalker130903@gmail.com>
2024-12-12 12:08:54 +0700

    minor fix

--- src/pages/Admin/DeliveryFeePage/index.jsx
+++ src/pages/Admin/DeliveryFeePage/index.jsx
@@ -158,11 +158,11 @@ const DeliveryFeePage = () => {
 					</Button>
 					<Button
 						onClick={() => HandleActiveDeliveryFee(record.Id)}
-						danger
+						
 						className={
 							record.IsEnabled
 								? 'text-red hover:text-redLight'
-								: 'text-green hover:text-greenDark'
+								: 'text-green hover:text-green'
 						}
 						icon={
 							record.IsEnabled ? (
